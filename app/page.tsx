3511764d877287cf8fbf569cef3e6fa3493bac67
Harry Vu <harryvu@outlook.com>
2023-06-14 00:54:21 -0700

    added random post title to the SSR header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image'
 
+async function getRandomPost() {
+  // random number between 1 and 100
+  const randomId = Math.floor(Math.random() * 100)
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${randomId}`, { cache: "no-cache" })
+  return await res.json()
+}
+
 async function getServerTime() {
   const date = new Date().toLocaleString()
   return { props: { date } };
@@ -7,9 +14,12 @@ async function getServerTime() {
 
 export default async function Home() {
   const serverTime = await getServerTime()
+  const randomPost = await getRandomPost()
+  console.log(randomPost.title)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>{serverTime.props.date}</div>
+      <div>{randomPost.title}</div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
