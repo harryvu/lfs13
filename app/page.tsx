c2ca8e249086075c16af892a66eb884f96619d9a
Harry Vu <harryvu@outlook.com>
2023-06-23 12:44:49 -0700

    Adds prisma and sqlite3 and todo list.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function getTodos() {
+  // random number between 1 and 100
+  const todos = await prisma.todo.findMany()
+  return todos
+}
+
 
 async function getRandomPost() {
   // random number between 1 and 100
@@ -13,13 +23,21 @@ async function getServerTime() {
 }
 
 export default async function Home() {
+  const todos = await getTodos()
+  console.log(todos)
+
   const serverTime = await getServerTime()
   const randomPost = await getRandomPost()
   console.log(randomPost.title)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>{serverTime.props.date}</div>
-      <div>{randomPost.title}</div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+      
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
