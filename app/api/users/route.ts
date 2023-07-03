209660f1641e354b194b5ea4b2ee86ba5c66b3de
Harry Vu <harryvu@outlook.com>
2023-07-03 08:07:28 -0700

    yat500

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,51 +5,52 @@ import * as dateFn from "date-fns";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    // return NextResponse.json({ message: "Hello World" });
-    const formData = await request.formData();
+    console.log(process.cwd());
+    return NextResponse.json({ cwd: process.cwd() });
+    // const formData = await request.formData();
 
-    const file = formData.get("file") as Blob | null;
-    if (!file) {
-        return NextResponse.json(
-            { error: "File blob is required." },
-            { status: 400 }
-        );
-    }
+    // const file = formData.get("file") as Blob | null;
+    // if (!file) {
+    //     return NextResponse.json(
+    //         { error: "File blob is required." },
+    //         { status: 400 }
+    //     );
+    // }
 
-    const buffer = Buffer.from(await file.arrayBuffer());
-    const relativeUploadDir = `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
-    const uploadDir = join(process.cwd(), "public", relativeUploadDir);
+    // const buffer = Buffer.from(await file.arrayBuffer());
+    // const relativeUploadDir = `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
+    // const uploadDir = join(process.cwd(), "public", relativeUploadDir);
 
-    try {
-        await stat(uploadDir);
-    } catch (e: any) {
-        if (e.code === "ENOENT") {
-        await mkdir(uploadDir, { recursive: true });
-        } else {
-        console.error(
-            "Error while trying to create directory when uploading a file\n",
-            e
-        );
-        return NextResponse.json(
-            { error: `Error while trying to create directory when uploading a file!\n${e}` },
-            { status: 500 }
-        );
-        }
-    }
+    // try {
+    //     await stat(uploadDir);
+    // } catch (e: any) {
+    //     if (e.code === "ENOENT") {
+    //     await mkdir(uploadDir, { recursive: true });
+    //     } else {
+    //     console.error(
+    //         "Error while trying to create directory when uploading a file\n",
+    //         e
+    //     );
+    //     return NextResponse.json(
+    //         { error: `Error while trying to create directory when uploading a file!\n${e}` },
+    //         { status: 500 }
+    //     );
+    //     }
+    // }
 
-    try {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-        const filename = `${file.name.replace(
-        /\.[^/.]+$/,
-        ""
-        )}-${uniqueSuffix}.${mime.getExtension(file.type)}`;
-        await writeFile(`${uploadDir}/${filename}`, buffer);
-        return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
-    } catch (e) {
-        console.error("Error while trying to upload a file\n", e);
-        return NextResponse.json(
-        { error: `Something went wrong!\n${e}` },
-        { status: 500 }
-        );
-    }
+    // try {
+    //     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    //     const filename = `${file.name.replace(
+    //     /\.[^/.]+$/,
+    //     ""
+    //     )}-${uniqueSuffix}.${mime.getExtension(file.type)}`;
+    //     await writeFile(`${uploadDir}/${filename}`, buffer);
+    //     return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
+    // } catch (e) {
+    //     console.error("Error while trying to upload a file\n", e);
+    //     return NextResponse.json(
+    //     { error: `Something went wrong!\n${e}` },
+    //     { status: 500 }
+    //     );
+    // }
 }
